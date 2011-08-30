a2fc49570ae051daf7fb9c1955bfad19a12cc386
Silvio Brugada <silviob@gmail.com>
2011-08-29 20:17:55 -0700

    Factored out the test utils into its own file

--- room.js
+++ room.js
@@ -2,17 +2,9 @@
 
   var inner = {};
   var resources = {};
-  var loopbackStore = { data: {}, parent: null };
-
-  var printStore = function(store, level) {
-    store = store || loopbackStore;
-    level = level || '';
-    for(var i in store.data) {
-      console.log(level + i);
-      printStore(store.data[i], level + '  ');
-    }
-  }
-
+  
+  inner.ajax = jQuery.ajax;
+  
   var identity = function(data) { 
     return data;
   };
@@ -58,74 +50,8 @@
   }
   inner.configurePackData('rails');
 
-  inner.updateDataFilter = identity;
-  inner.destroyDataFilter = identity;
-
-  var makeLeaf = function(data, parent) {
-    return { data: $.extend(true, {}, data),
-             parent: parent};
-  };
-
-  var getOrCreateLeaf = function(path, store, enableCreate) {
-    var parts = path.split('/');
-    var zeroth = parts[0];
-    parts = parts.slice(1);
-    if(zeroth === '') return getOrCreateLeaf(parts.join('/'), store, enableCreate);
-    if(enableCreate && !store.data[zeroth]) store.data[zeroth] = makeLeaf({}, store);
-    if(parts.length == 0) return store.data[zeroth];
-    return getOrCreateLeaf(parts.join('/'), store.data[zeroth], enableCreate);
-  };
-
-  var loopback = function(options) {
-    var re = new RegExp('\.' + inner.extension + '$');
-    options.url = options.url.replace(re, '');
-    options.type = options.type || 'get';
-    switch(options.type.toLowerCase()) {
-    case 'post':
-      var store = getOrCreateLeaf(options.url, loopbackStore, true);
-      var stored = 0;
-      for(var i in store) { stored++ };
-      store.data[stored] = makeLeaf(options.data, store);
-      options.success(store.data[stored].data);
-      break;
-    case 'get':
-      var store = getOrCreateLeaf(options.url, loopbackStore, false);
-      console.log('getting');
-      printStore(store);
-      options.success(store.data);
-      break;
-    case 'delete':
-      var store = getOrCreateLeaf(options.url, loopbackStore, false);
-      if(!store) { 
-        options.error();
-      } else {
-        var id = options.url.slice(options.url.lastIndexOf('/') + 1);
-        var data = store.data;
-        var parent = store.parent;
-        if(parent.data[id] && delete parent.data[id])
-          options.success(data);
-        else
-          options.error();
-      }
-      break;
-    }
-    printStore();
-  }
-
-  inner.enterLoopbackMode = function() {
-    inner.ajax = loopback;
-  }
-
-  inner.leaveLoopbackMode = function() {
-    inner.ajax = jQuery.ajax;
-  }
-
-  inner.leaveLoopbackMode();
-
   var getPath = function() {
-    var path = this.path + '.' + inner.extension;;
-    console.log('accesssing: ' + path);
-    return path;
+    return this.path + '.' + inner.extension;
   };
 
   var wrapSuccess = function(type, successCallback, options) {
@@ -231,6 +157,6 @@
     });
   };
 
-})(jQuery);
+}(jQuery));
 
 $.room().initFromMetaTags();
